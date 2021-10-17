feb8db11831114858a3387a1d4518289d33111c5
Andrew Monk <andrew.m.monk@gmail.com>
2021-10-17 11:57:14 -0700

    Modified BOOT.SH to export port 8080

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec gunicorn -b :8000 --access-logfile - --error-logfile - flaskweb:app
+exec gunicorn -b :8080 --access-logfile - --error-logfile - flaskweb:app
--- test.py
+++
@@ -1,4 +0,0 @@
-from Entities.entities import Entities
-
-if __name__ == "__main__":
-    e = Entities()
